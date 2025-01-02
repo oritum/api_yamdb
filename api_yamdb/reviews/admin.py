f831be813264b7b8e672d20111020b1ffc6191af
Valeria Madanova <v.madanova@outlook.com>
2025-01-02 22:55:18 +0300

    add models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Review, Comment
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Админка для Review."""
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'score',
+        'pub_date'
+    )
+    list_filter = (
+        'author',
+        'pub_date',
+    )
+    search_fields = (
+        'text',
+        'author__username'
+    )
+    ordering = ('-pub_date',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Админка для Comment."""
+    list_display = (
+        'review',
+        'text',
+        'author',
+        'pub_date'
+    )
+    list_filter = (
+        'author',
+        'pub_date'
+    )
+    search_fields = (
+        'text',
+        'author__username'
+    )
+    ordering = ('-pub_date',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,82 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Title(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Review(models.Model):
+    """Модель для отзывов."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(verbose_name='Текст отзыва')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        choices=[(i, i) for i in range(1, 11)]
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author', ),
+                name='one_review'
+            )]
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        return f'Отзыв от {self.author} на {self.title}'
+
+
+class Comment(models.Model):
+    """Модель для комментариев к отзывам."""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'{self.author}: {self.review}'
