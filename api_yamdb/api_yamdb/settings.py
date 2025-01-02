81a82339cd3e9f9aba639fc5b72b8111e4bbc9e5
Oleg Ritum <oritum@gmail.com>
2025-01-02 21:47:15 +0000

    delete email util

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,22 +13,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = config('EMAIL_HOST')
-
-EMAIL_PORT = config('EMAIL_PORT')
-
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
-
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
