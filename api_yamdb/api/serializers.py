565c1fb23487b63776203ccd59f1fa6cd93bc8bb
Valeria Madanova <v.madanova@outlook.com>
2025-01-06 21:37:19 +0300

    add views and serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -7,9 +7,12 @@ from rest_framework.serializers import (
     EmailField,
     ModelSerializer,
     ValidationError,
+    SlugRelatedField,
+    ValidationError,
+    CurrentUserDefault,
 )
 
-from reviews.models import User
+from reviews.models import User, Review, Comment
 from users.constants import EMAIL_MAX_LENGTH, USERNAME_MAX_LENGTH
 from users.validators import validate_username
 
@@ -93,3 +96,45 @@ class CustomTokenObtainSerializer(BaseUserSerializer):
                 {'confiration_code': 'неверный confirmation_code'}
             )
         return data
+
+
+class ReviewSerializer(ModelSerializer):
+    """Серилизатор для оценка произведений."""
+    title = SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = SlugRelatedField(
+        slug_field='username',
+        default=CurrentUserDefault(),
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('author', 'title',)
+
+    def validate_score(self, value):
+        if not 1 <= value <= 10:
+            raise ValidationError(
+                'Оценка должна быть от 1 до 10'
+            )
+        return value
+
+
+class CommentSerializer(ModelSerializer):
+    """Серилизатор для комментариев на отзыв."""
+    review = SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('review',)
