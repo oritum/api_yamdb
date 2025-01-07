eeb759b874f80bb1dab1a39558338288810d1b55
Valeria Madanova <v.madanova@outlook.com>
2025-01-07 15:50:44 +0300

    fix review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -106,13 +106,14 @@ class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthorOrReadOnly, IsModeratorAdminPermission)
 
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
     def get_queryset(self):
-        return get_object_or_404(
-            Title, id=self.kwargs.get('title_id')).reviews.all()
+        return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        serializer.save(author=self.request.user, title=title)
+        serializer.save(author=self.request.user, title=self.get_title())
 
 
 class CommentViewSet(ModelViewSet):
@@ -120,10 +121,11 @@ class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly, IsModeratorAdminPermission)
 
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+
     def get_queryset(self):
-        return get_object_or_404(
-            Review, id=self.kwargs.get('review_id')).comments.all()
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review)
+        serializer.save(author=self.request.user, review=self.get_review())
