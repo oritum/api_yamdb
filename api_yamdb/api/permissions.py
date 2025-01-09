74699a0da55c2e9d2a52a234b32d1d19b0598fa4
Valeria Madanova <v.madanova@outlook.com>
2025-01-09 21:22:20 +0300

    fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,16 +13,6 @@ class AdminOnlyPermission(BasePermission):
         return request.user.is_staff or request.user.is_admin
 
 
-class IsAuthorOrReadOnly(BasePermission):
-    """
-    Доступ для автора удялять, изменять свои отзывы и комментарии,
-    чтение всем.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return request.method in SAFE_METHODS or obj.author == request.user
-
-
 class IsModeratorAdminPermission(BasePermission):
     """
     Доступ для админа и модератора удялять, изменять
